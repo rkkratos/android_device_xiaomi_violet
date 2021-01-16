5c015decc693afcc3cace8e5dcdde6311404a139
shashank1436 <9945shashank@gmail.com>
2021-01-16 08:04:52 +0100

    violet: Enable logcat service
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -388,7 +388,9 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    logcat.rc
+
 
 # RCS
 PRODUCT_PACKAGES += \
