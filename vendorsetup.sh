bd5c5bac48ca323e6c63380ce7c4d8f687bf3928
Bruno Martins <bgcngm@gmail.com>
2024-03-28 13:11:44 +0000

    violet: Declare EGL libs as symlinks during extraction
    Change-Id: I4b2f3e79b5e518889d9771bf8990e27babca8b61

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
+git clone https://gitlab.com/Joker-V2/vendor_xiaomi-firmware_violet.git -b 15 vendor/xiaomi-firmware/violet
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
+git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
+git clone https://github.com/ProjectPixelage/android_hardware_xiaomi.git -b 15 hardware/xiaomi
