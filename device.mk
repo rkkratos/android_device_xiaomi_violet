34991d8caa71a9c395d8a6671cb158eb8580b65c
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 13:25:04 +0300

    violet: Build sound trigger HAL from source
    Change-Id: Iad70929cd9c8f7b62683d6a8488a7593d549885c
    Signed-off-by: pppig236 <priv@pppig236.com>
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -120,7 +120,8 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libtinycompress \
-    libvolumelistener
+    libvolumelistener \
+    sound_trigger.primary.sm6150:32
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
