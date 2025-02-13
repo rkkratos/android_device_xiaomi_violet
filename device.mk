4d0ea2ea537045181ac6a1dde7149487bb812c59
rkkratos <rkriyon2@gmail.com>
2025-02-13 14:25:44 +0000

    Revert "sm6150-common: media: Import media_codecs.xml"
    This reverts commit fa8721f5eac89e17845a1e2c7780f27a34393422.

--- device.mk
+++ device.mk
@@ -332,7 +332,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
