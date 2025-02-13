b76a09aede99890568cd208c2c990d23f2abdd9b
rkkratos <rkriyon2@gmail.com>
2025-02-13 14:26:47 +0000

    Revert "violet: remove OMX"
    This reverts commit a87adc81f71f11c1254a16e3add8333f811c2824.

--- device.mk
+++ device.mk
@@ -329,6 +329,15 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
+    libmm-omxcore \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \
