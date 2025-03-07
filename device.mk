a518ecd375137661cf4271b096f7b3bf80e5c66e
rkkratos <rkriyon2@gmail.com>
2025-03-07 05:13:05 +0000

    violet: added missing hw dir

--- device.mk
+++ device.mk
@@ -444,6 +444,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
